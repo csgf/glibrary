61bd3098560edc2604998c33996c1c1ce04ab007
hellbreak <antonio.dimariano@gmail.com>
2015-10-30 17:36:27 +0100

    FIXED coll_data.import check for boolean or string value

--- common/helpers/loadModel.js
+++ common/helpers/loadModel.js
@@ -689,7 +689,7 @@ module.exports = function (app) {
                   var db_name = setDB_name(app, coll_data);
                 }
 
-                if (coll_data.import == "true") {
+                if (coll_data.import == "true" || coll_data.import == true ) {
                   logger.debug("-------[getCollection][Import Data]----------");
                   var modelTable = coll_data.location;
                   var modelName = modelTable;
@@ -710,7 +710,7 @@ module.exports = function (app) {
                   app.CollectionDataSource, function (model) {
                     if (model) {
                       logger.debug("[getCollection][buildModelfromTable][OK]");
-                      if (coll_data.import == "true") {
+                      if (coll_data.import == "true" || coll_data.import == true ) {
                         var modelName = setModelName(app, req.params);
                         ModelTableMap[modelName] = {table: modelTable};
                         logger.stream.write("[getCollection]ModelTableMap[" + modelName + "]={table:" + modelTable + "}")
